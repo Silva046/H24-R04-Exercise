d9c5b904b0c6595373fe3e62883e165a63f1c11a
Silva046 <2037677@cegepmontpetit.ca>
2024-02-03 11:58:58 -0500

    Exercice #6 fini et création de Ex6 Compétences Exiges.csv

--- R03 Exercices_vincent_baccichet/Ex6 csv List et for.py
+++ R03 Exercices_vincent_baccichet/Ex6 csv List et for.py
@@ -1,6 +1,6 @@
 import os                             # N'enlevez pas ces lignes.
 os.chdir(os.path.dirname(__file__))   # Elles permettent de se positionner dans le répertoire de ce script
-
+os.chdir("csvs")
 import csv
 
 
@@ -8,7 +8,7 @@ import csv
 # Tu as vu une job en TI, 
 #           Analyste, soutien technique réseau
 
-# Le fichier Ex3 Competences.csv contient la liste des compétences qu'ils demandent, 
+# Le fichier Competences.csv contient la liste des compétences qu'ils demandent, 
 # #         avec le niveau et le fait que cette compétence est exigée ou plutôt un atout
 # Bien que tu n'as pas encore fini tes études tu veux imprimer ici les compétences qui sont exigées 
 # afin de bien développer ces compétences pour qu'éventuellement tu puisses entrer dans cette entreprise
@@ -16,6 +16,15 @@ import csv
 #  Si vous êtes à l'aise en programmation allez-y
 #  Des instructions détaillées sont données plus bas
 
+with open("Ex6 Competences.csv" , encoding='utf-8') as csv_file :
+    csv_reader = csv.reader(csv_file , delimiter='/')
+    with open("Ex6 Compétences Exiges.csv" , "w", encoding='utf-8') as csv_file_writer :
+        csv_writer = csv.writer(csv_file_writer , delimiter='/' , lineterminator='\n')
+        csv_writer.writerow(["Compétences" , "Niveau" , "Éxigences"])
+        next(csv_reader)
+        for line in csv_reader :
+            if line[2] in ["Exigé"] :
+                csv_writer.writerow(line)
 
 
 
