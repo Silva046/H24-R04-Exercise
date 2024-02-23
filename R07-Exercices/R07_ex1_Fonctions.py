90f91fdbfa82ff7abc1d98d8cc1c7115e56c155f
Silva046 <2037677@cegepmontpetit.ca>
2024-02-23 14:07:39 -0500

    R07 commencer

--- R07-Exercices/R07_ex1_Fonctions.py
+++ R07-Exercices/R07_ex1_Fonctions.py
@@ -4,11 +4,12 @@
 # Q1 A:  Définir une fonction sans paramètre                                    #
 #        Faites une fonction qui va imprimer "Bonjour" 
 #        
-
+def Bonjour():
+    print("Bonjour!")
 
 print(f"Q1{'_'*60}")
 # Q1 B:   Appellez cette fonction  
-
+Bonjour()
 
 
 
@@ -17,16 +18,17 @@ print(f"Q2{'_'*60}")
 #         Faites une fonction qui va imprimer "Bonjour XXXXXX" où XXXXXX est le nom passé en paramètre lors de l'appel de cette fonction
 #         Appelez-la bonjour_toi
 
- 
+def Bonjour_toi(nom_personne):
+    print(f"Bonjour {nom_personne}")
  
 
 # Q2 B:  Appel de la fonction bonjour_toi avec en paramètre le nom de mon ami Pierre
-
+Bonjour_toi("Pierre")
 
 
 # Q2 C:  Appel de la fonction bonjour_toi avec en paramètre le nom de mon amie Sylvie
 
-
+Bonjour_toi("Sylvie")
 
 
 print(f"Q3{'_'*60}")
@@ -34,12 +36,14 @@ print(f"Q3{'_'*60}")
 #        Faites une fonction qui va imprimer "Bonjour" et qui va retourner le message "Cela va?"
 #        Appelez-la cela_va
 
-
+def cela_va():
+    print("Bonjour")
+    return print("Cela va?")
 
 
 # Q3 B:   Appel de la fonction et capture de la réponse
 
-
+cela_va()
 
 
 print(f"Q4{'_'*60}")
@@ -48,12 +52,14 @@ print(f"Q4{'_'*60}")
 # 		 et qui va retourner le message "Je suis libre ce soir, et toi?"
 #        Appelez-la cela_va_bien
 
-
+def cela_va_bien(nom_personne):
+    print(f"Bonjour {nom_personne}")
+    return print("Je suis libre ce soir, et toi")
 
 
 
 # Q4 B:   Appel de la fonction et capture de la réponse
 
-
+cela_va_bien("Vincent")
 
 
--- R07-Exercices/R07_ex2_Invitations.py
+++ R07-Exercices/R07_ex2_Invitations.py
@@ -20,7 +20,9 @@ print("Bonjour Thomas, \n Je veux t'inviter à venir chez moi demain vers 18h00.
 print(f"Q1{'_'*60}")
 # Q1 A:  Faites une fonction pour inviter tout le monde, sans duplication du code.
 #        Cette fonction prendra une liste d'amis en paramètres                                      #
-
+def invitation(nom_liste):
+    for ami in nom_liste :
+        print(f"Bonjour {ami},\n Je veux t'inviter à venir chez moi demain vers 18h00. Cela t'intéresse?")
   
   
   
@@ -29,19 +31,23 @@ print(f"Q1{'_'*60}")
 # Q1 B:   Appellez la fonction avec la liste d'amis qu'on veut inviter. 
 amis = ["Marc", "Pierre", "Lucie", "Achraf","Claude", "Ilyan", "Maria", "Thomas"]
 
-
+invitation(amis)
 
 
 
 print(f"Q2{'_'*60}")
 # Q2 A:   Faites une nouvelle fonction pour inviter tout le monde, dans un lieu, un jour et une heure qui varient, sans duplication du code
 #         Appelez cette fonction invitation2 (elle devrait prendre 4 paramètres)
+def invitation_precise(nom_liste , lieu , jour , heure):
+    for ami in nom_liste :
+        print(f"Bonjour {ami},\n Je veux t'inviter à venir {lieu} {jour} vers {heure}. Cela t'intéresse?")
 
+liste_amis = ["Sylvain" , "Nathalie" , "Paul"]
 
 # Q2 B:  Appel de la fonction invitation2 pour inviter Sylvain et Nathalie au café étudiant, vendredi, vers 13h00
-
+invitation_precise(liste_amis[0:2] , "au café étudiant" , "Vendredi" , "13h00")
 
 # Q2 C:  Appel de la fonction invitation2 pour inviter Sylvain, Nathalie et Paul chez moi pour souper, samedi vers 17h00
 
-
+invitation_precise(liste_amis , "chex moi" , "samedi" , "17h00")
 
