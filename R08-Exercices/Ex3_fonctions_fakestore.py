fb542532024e8e503205a854fb0d512d38f445f5
Silva046 <2037677@cegepmontpetit.ca>
2024-03-02 15:37:03 -0500

    fini R08

--- R08-Exercices/Ex3_fonctions_fakestore.py
+++ R08-Exercices/Ex3_fonctions_fakestore.py
@@ -14,9 +14,15 @@ base_url="https://fakestoreapi.com"
 
 #   La fonction doit retourner les carts obtenu avec la requête get du site webs. Mais seulement si le nombre de carts demandés est entre 1 et 10
 
+def request_carts(nombre:int=1):
+    if 1 <= nombre <= 10:
+        res = requests.get(f"{base_url}/carts?limit={nombre}")
+        return res.json()
+    else:
+        print(f"Il faut demander entre 1 et 10 carts et non {nombre} carts!")
 
 
-
+print(request_carts(2))
 
 
 # Q2 Écrivez une fonction appelée request_products qui aura 1 paramètre
@@ -29,9 +35,23 @@ base_url="https://fakestoreapi.com"
 
 #   La fonction doit retourner le nombre de produits demandés, si le nombre est entre 1 et 10
 
+def valider_nombre(nombre:int):
+    if nombre == None:
+        print(int(input("Entré un nombre entre 1 et 10 : ")))
+    elif nombre < 1:
+        nombre = 1
+    elif nombre > 10:
+        nombre = 10
+    return nombre
 
 
+def request_products(nombre:int=None):
+    nombre = valider_nombre(nombre)
+    res = requests.get(f"{base_url}/products?limit={nombre}")
+    return res.json()
 
+
+print(json.dumps(request_products(),indent=4))
 
 
 
