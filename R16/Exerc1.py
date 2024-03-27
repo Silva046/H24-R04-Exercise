5910de203d623696c08ef432671a63073c93716e
Silva046 <2037677@cegepmontpetit.ca>
2024-03-27 11:46:32 -0400

    commencer R16

---
+++ R16/Exerc1.py
@@ -0,0 +1,18 @@
+class Departement:
+    def __init__(self,id,nom,budget_annuel) -> None:
+        self.id = id
+        self.nom = nom
+        self.budget_annuel = budget_annuel
+
+class Achat:
+    def __init__(self,id,montant_total,est_paye,equipement) -> None:
+        self.id = id
+        self.montant_total = montant_total
+        self.est_paye = est_paye
+        self.equipement = equipement
+
+    def calculer_total_estimer():
+        pass
+    def payer():
+        pass
+
