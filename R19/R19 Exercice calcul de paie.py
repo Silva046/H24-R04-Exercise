95e6324a1be0241075f983903096bc83f569ae80
Silva046 <2037677@cegepmontpetit.ca>
2024-04-18 19:57:46 -0400

    debut r19

--- R19/R19 Exercice calcul de paie.py
+++ R19/R19 Exercice calcul de paie.py
@@ -8,22 +8,57 @@
 from abc import ABC,abstractmethod
 
 class Systeme_de_paie:
-    pass
-            
+    def afficher_paies(liste_employe:list=str):
+        for employes in liste_employe:
+            print(f"voici la paie de {employes} : {Employe.calculer_paie()}")
+
+     
 class Employe(ABC):
-    pass
+    def __init__(self, id_employe, nom) -> None:
+        super().__init__()
+        self.id_employe = id_employe
+        self.nom = nom
+    
+    @abstractmethod
+    def calculer_paie(self):
+        pass
     
 class Employe_salarie(Employe):
-    pass
+    def __init__(self, id_employe, nom, salaire_par_semaine:int) -> None:
+        super().__init__(id_employe, nom)
+        self.salaire_par_semaine = salaire_par_semaine
+
+    def calculer_paie(self):
+        return self.salaire_par_semaine
  
 class Employe_heure(Employe):  
-   pass
+   def __init__(self, id_employe, nom, heures_travailles:int, taux_horaire:int) -> None:
+       super().__init__(id_employe, nom)
+       self.heures_travailles = heures_travailles
+       self.taux_horaire = taux_horaire
+   
+   def calculer_paie(self):
+       return self.heures_travailles * self.taux_horaire
     
 class Employe_Commission(Employe_salarie):  
-    pass
+    def __init__(self, id_employe, nom, salaire_par_semaine:int, commission) -> None:
+        super().__init__(id_employe, nom, salaire_par_semaine)
+        self.commission = commission
+
+    def calculer_paie(self):
+        return self.salaire_par_semaine + self.commission
  
 
+#test
 
+employe_salaire = Employe_salarie(1,"Marc Tremblay",2100)
+employe_heure = Employe_heure(2,"Pierre Jonhson",40,22)
+employe_commission = Employe_Commission(3,"Luc Toupin",1400,600)
+liste_des_employes = []
+liste_des_employes.append(employe_salaire)
+liste_des_employes.append(employe_heure)
+liste_des_employes.append(employe_commission)
+Systeme_de_paie.afficher_paies(liste_des_employes)
 #instanciation des objets 
 if __name__ == "__main__": pass
     
