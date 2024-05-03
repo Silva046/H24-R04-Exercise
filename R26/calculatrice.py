8218dba4a5469a8efa590ed4a898ebfccb46097c
Silva046 <2037677@cegepmontpetit.ca>
2024-05-03 14:37:24 -0400

    avancer R26

--- R26/calculatrice.py
+++ R26/calculatrice.py
@@ -37,7 +37,7 @@ class App(customtkinter.CTk):
         ## Autres btn
         self.plus = customtkinter.CTkButton(self.btn_frame,text='+',height=100,width=100,fg_color='grey',command=lambda : self.ajout_formule(' + '),font=('',40)).grid(row=0,column=3)
         self.moins = customtkinter.CTkButton(self.btn_frame,text='-',height=100,width=100,fg_color='grey',command=lambda : self.ajout_formule(' - '),font=('',40)).grid(row=1,column=3)
-        self.multiple = customtkinter.CTkButton(self.btn_frame,text='x',height=100,width=100,fg_color='grey',command=lambda : self.ajout_formule(' x '),font=('',40),state='disabled').grid(row=2,column=3)
+        self.multiple = customtkinter.CTkButton(self.btn_frame,text='x',height=100,width=100,fg_color='grey',command=lambda : self.ajout_formule(' x '),font=('',40),state='enabled').grid(row=2,column=3)
         self.egal = customtkinter.CTkButton(self.btn_frame,text='=',height=100,width=100,fg_color='green',command=self.resoudre_equation,font=('',40)).grid(row=3,column=3)
         self.delete = customtkinter.CTkButton(self.btn_frame,text='del',height=100,width=100,fg_color='red',command=self.effacer_ecran,font=('',40)).grid(row=3,column=2)
 
@@ -46,31 +46,41 @@ class App(customtkinter.CTk):
     def ajout_formule(self,caractere):
         # Obtenez la longueur de la chaine de caractère dans self.ecran
         # Insérez le caractère à la fin du self.ecran 
-        pass
+        longueur = len(self.ecran.get())
+        self.ecran.insert(longueur,caractere)
 
     # Résous l'équation afficher dans self.ecran
     def resoudre_equation(self):
         # Appelez la méthode lire_equation et capturez le retour
         # Effacez le contenue de self.ecran
         # Affichez le nouveau résultat
-        pass
+        self.lire_equation()
 
     # Lis l'équation de gauche à droite, sans priorité d'opération.
     # Retourne le résultat de l'opération
     def lire_equation(self):
         # Séparez l'équation en différentes parties avec split(), entreposé le résultat dans une variable.
+        equation = self.ecran.get().split(" ")
         # Initialisé une variable totale à la valeur du premier élément de la liste
+        totale = int(equation[0])
+        for i in range(1, len(equation),2):
+            if equation[i] == "+":
+                totale += int(equation[i+1])
+            elif equation[i] == "-":
+                totale -= int(equation[i+1])
+            elif equation[i] == "x":
+                totale *= int(equation[i+1])
         # Passez au travers de la liste en utilisant la fonction range()
         #       cette fonction retourne l'index de chaque élément.
         #       pour chaque élément, regarder s'il s'agit d'un "+", d'un "-", ou autre
         #           si c'est un "+" ; additionnez le prochain élément de la liste au total
         #           si c'est un "-" ; soustrayez le prochain élément de la liste au total
 
-        return 0
+        return print(totale)
 
     # Efface le contenu de l'écran self.ecran
     def effacer_ecran(self):
-        pass
+        self.ecran.delete("0","end")
 
 if __name__ == "__main__":
     app = App()
