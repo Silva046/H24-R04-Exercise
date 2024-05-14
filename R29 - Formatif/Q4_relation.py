752def7f38a7cd2b4008c493c4d68753e8090896
Silva046 <2037677@cegepmontpetit.ca>
2024-05-14 17:41:41 -0400

    fini formatif

--- R29 - Formatif/Q4_relation.py
+++ R29 - Formatif/Q4_relation.py
@@ -1,16 +1,38 @@
 from datetime import date
 
 
-class Patient: pass
-
+class Patient:
+    def __init__(self, id:int, nom:str="inconnue", NAS:int|str="inconnue") -> None:
+        self.id = id
+        self.nom = nom
+        self.NAS = NAS
+        self.date_admission = None
 class Departement:
-    def __init__(self): pass
+    liste_departement = []
+    def __init__(self, nom:str):
+        self.nom = nom
+        self._liste_patients = []
+        Departement.liste_departement.append(self)
 
+    @property
+    def liste_patients(self):
+        return self._liste_patients
 
-    def ajouter_patient(self): pass
+    def ajouter_patient(self, patient:Patient):
+        patient.date_admission = date.today()
+        self._liste_patients.append(patient)
+        if patient.nom == "inconnue":
+            return print(f"le patient {patient.id} a bien été ajouté")
+        else: return print(f"le patient {patient.nom} a bien été ajouté")
 
-
-    def imprimer_liste_des_patients(cls): pass
+    @classmethod
+    def imprimer_liste_des_patients(cls): 
+        dep:Departement
+        for dep in cls.liste_departement:
+            print(f"Voici la liste des départements {dep.nom}")
+            pat:Patient
+            for pat in dep._liste_patients:
+                print(f"\t{pat.id} {pat.nom} {pat.date_admission}")
 
 
 if __name__ == "__main__" :
